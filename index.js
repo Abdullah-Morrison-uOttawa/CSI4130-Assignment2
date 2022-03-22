d496ebcb3889516fd6cc6d44ea443e245dbd18c7
Steven Sun <jiasun0916@gmail.com>
2022-03-22 17:44:52 -0400

    Cleaned some screen clutter

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
 function init() {
-    var container = document.createElement('div');
-    document.body.appendChild(container);
 
-    document.body.appendChild(container);
-    info = document.createElement('div');
-    info.style.position = 'absolute';
-    info.style.top = '5px';
-    info.style.left = '5px';
-    info.style.width = '100%';
-    info.style.textAlign = 'left';
-    info.style.color = "lightblue";
-    info.innerHTML = "row 0<br>row 1<br>row 2<br>row 3";
-    container.appendChild(info);
-
-    renderer = new THREE.WebGLRenderer();
+    renderer = new THREE.WebGLRenderer({ antialias: true });
     // set some state - here just clear color
     renderer.setClearColor(new THREE.Color(0x333333));
     renderer.setSize(window.innerWidth, window.innerHeight);
     // add the output of the renderer to the html element
-    container.appendChild(renderer.domElement);
+    document.body.appendChild(renderer.domElement);
 
 
     // All drawing will be organized in a scene graph
@@ -31,7 +18,7 @@ function init() {
     scene.add(axes);
 
     // Adding teapot
-    var teapotGeometry = new THREE.TeapotGeometry(5, 15, true, true, true, false, false);
+    var teapotGeometry = new THREE.TeapotGeometry(5, 20, true, true, true, false, false);
     var teapot = new THREE.Mesh(teapotGeometry, new THREE.MeshBasicMaterial({ color: 'pink' }));
     teapot.position.set(0, 0, 0);
     scene.add(teapot);
@@ -45,7 +32,6 @@ function init() {
     camera.position.y = 45 
     camera.position.z = 200;
     camera.lookAt(scene.position);
-    camera.position.y = 55 
 
     // render the scene
     renderer.render(scene, camera);
@@ -86,14 +72,15 @@ function init() {
 
 }
 
+window.onload = init;
+
 function onResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
     // If we use a canvas then we also have to worry of resizing it
     renderer.setSize(window.innerWidth, window.innerHeight);
-}
 
-window.onload = init;
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+}
 
 // register our resize event function
 window.addEventListener('resize', onResize, true);
